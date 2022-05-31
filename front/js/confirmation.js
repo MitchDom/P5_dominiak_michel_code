c375e3d92ab46e130175a493c8092d0393f6118e
MitchDom <dominiak.michel@gmail.com>
2022-05-31 16:38:19 +0200

    remplacement de var par const

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,5 @@
 //Fonction pour récuperer le numéro de commande dans l'URL
- var orderId = document.location.href.substring(document.location.href.lastIndexOf("?")+9);
+const orderId = document.location.href.substring(
+  document.location.href.lastIndexOf("?") + 9
+);
 document.getElementById("orderId").innerText = orderId;
